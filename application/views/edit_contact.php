6c6bbecd885a7decee3187a42741cd50fed80b3c
Faizan Ali <sh.faizan.ali@hotmail.com>
2013-03-16 23:38:01 +0500

    asd

--- application/views/edit_contact.php
+++ application/views/edit_contact.php
@@ -6,17 +6,22 @@ if($success) {
 <script>
     $(function(){
        var company_type_selected = $('#companies').val();
-       $.ajax({
-           url:'<?= base_url()?>csv/get_company_name_dropdowns?company_type='+company_type_selected+'&company_id='+<?=$contact->company_id?>,
-           type:'GET',
-            success: function(data){
-                $('#companies_names').html(data);
-            } 
-       });
-       
-       $('#companies').live('change',function(e){
-           console.log('here');
+
+       $('#companies').live('change',function(e) {
             company_type_selected = $('#companies').val();
+            if(company_type_selected != 1){
+                $('#lien_position').hide();
+                $('#section').hide();
+                $('#department').hide();
+                $('#loan_type').hide();
+            }
+            else {
+                $('#lien_position').show();
+                $('#section').show();
+                $('#department').show();
+                $('#loan_type').show();
+
+            }
             $.ajax({
                url:'<?= base_url()?>csv/get_company_name_dropdowns?company_type='+company_type_selected,
                type:'GET',
@@ -25,6 +30,7 @@ if($success) {
                 } 
             }); 
        });
+       
        $('form').on('submit',function(e){
             $('.alert').remove();
             e.preventDefault();
@@ -48,6 +54,17 @@ if($success) {
 </script>
     <form method="POST" action="<?= base_url()?>csv/do_edit_contact/<?=$contact->id?>" enctype='multipart/form-data'>
         <fieldset>
+        
+        <select name="companies" id='companies'>
+            <? 
+                foreach($companies as $key=>$value):
+            ?>
+                    <option value="<?=$value?>"> <?=$key?> </option>
+            <?
+                endforeach;
+            ?>
+        </select>
+        <br/>
         <select name="escalation_level">
             <? 
                 foreach($escalations as $key=>$value):
@@ -65,90 +82,85 @@ if($success) {
             ?>
         </select>
         <br/>
-        <select name="companies" id='companies'>
-            <? 
-                foreach($companies as $key=>$value):
-            ?>
-                    <option value="<?=$value?>"> <?=$key?> </option>
-            <?
-                endforeach;
-            ?>
-        </select>
-        <br/>
         <div id="companies_names">
             
         </div>
-        OR
-        <br/>
-        <input type="text" name="company_name_text" placeholder="Type the company name here" /> <br/>
-        <select name="lien_position">
-            <? 
-                foreach($lien_position as $key=>$value):
-                    if($value == $contact->lien_position){
-            ?>
-                    <option value="<?=$value?>" selected="selected"> <?=$key?> </option>
-            <?
-                    }
-                    else {
-            ?>
-                    <option value="<?=$value?>"> <?=$key?> </option>
-            <?
-                    }
-                endforeach;
-            ?>
-        </select>
-        <br/>
-        <select name="section">
-            <? 
-                foreach($section as $key=>$value):
-                    if($value == $contact->section_id){
-            ?>
-                    <option value="<?=$value?>" selected="selected"> <?=$key?> </option>
-            <?
-                    }
-                    else{
-            ?>
-                    <option value="<?=$value?>"> <?=$key?> </option>
-            <?
-                    }
-                endforeach;
-            ?>
-        </select>
-        <br/>
-        <select name="department">
-            <? 
-                foreach($department as $key=>$value):
-                    if ( $value == $contact->departmend_id){
-            ?>
+        <div id="lien_position">
+            <select name="lien_position">
+                <? 
+                    foreach($lien_position as $key=>$value):
+                        if($value == $contact->lien_position){
+                ?>
                         <option value="<?=$value?>" selected="selected"> <?=$key?> </option>
-            <?
-                    }
-                    else {
-            ?>
-                        <option value="<?=$value?>"> <?=$key?> </option>      
-            <?   
-                    }
-                endforeach;
-            ?>
-        </select>
-        <br/>
-        <select name="loan_type">
-            <? 
-                foreach($loan_type as $key=>$value):
-                    if($value == $contact->loan_type_id){
-            ?>
-                        <option value="<?=$value?>" selected="selected"> <?=$key?> </option>
-            <?
-                    }
-                    else {
-            ?>
+                <?
+                        }
+                        else {
+                ?>
                         <option value="<?=$value?>"> <?=$key?> </option>
-            <?            
-                    }
-                endforeach;
-            ?>
-        </select>
-        <br/>
+                <?
+                        }
+                    endforeach;
+                ?>
+            </select>
+            <br/>
+        </div>
+        <div id="section">
+            <select name="section">
+                <? 
+                    foreach($section as $key=>$value):
+                        if($value == $contact->section_id){
+                ?>
+                        <option value="<?=$value?>" selected="selected"> <?=$key?> </option>
+                <?
+                        }
+                        else{
+                ?>
+                        <option value="<?=$value?>"> <?=$key?> </option>
+                <?
+                        }
+                    endforeach;
+                ?>
+            </select>
+            <br/>
+        </div>
+        <div id="department">
+            <select name="department">
+                <? 
+                    foreach($department as $key=>$value):
+                        if ( $value == $contact->departmend_id){
+                ?>
+                            <option value="<?=$value?>" selected="selected"> <?=$key?> </option>
+                <?
+                        }
+                        else {
+                ?>
+                            <option value="<?=$value?>"> <?=$key?> </option>      
+                <?   
+                        }
+                    endforeach;
+                ?>
+            </select>
+            <br/>
+        </div>
+        <div id="loan_type">
+            <select name="loan_type">
+                <? 
+                    foreach($loan_type as $key=>$value):
+                        if($value == $contact->loan_type_id){
+                ?>
+                            <option value="<?=$value?>" selected="selected"> <?=$key?> </option>
+                <?
+                        }
+                        else {
+                ?>
+                            <option value="<?=$value?>"> <?=$key?> </option>
+                <?            
+                        }
+                    endforeach;
+                ?>
+            </select>
+            <br/>
+        </div>
         <input type="text" name="first_name" placeholder="First Name" value="<?=$contact->first_name?>"/> <br/>
         <input type="text" name="middle_name" placeholder="Middle Name" value="<?=$contact->suffix?>" /> <br/>
         <input type="text" name="last_name" placeholder="Last Name" value="<?=$contact->last_name?>" /> <br/>
